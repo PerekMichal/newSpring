c881ef463ac81a30d5b836fe7b6927f55a37d802
Perek Michal <perekmichal93@gmail.com>
2018-01-05 13:14:27 +0100

    Initial commit

---
+++ src/main/java/pl/perekmichal/newSpring/NewSpringApplication.java
@@ -0,0 +1,12 @@
+package pl.perekmichal.newSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/perekmichal/newSpring/NewSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.perekmichal.newSpring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
